897edb68fd76e4da25ff418466399b1488b14c48
jinghongyujian2 <jinghongyujian@gmail.com>
2026-01-21 17:42:21 +0800

    Add Python exercises

---
+++ exercises.py
@@ -0,0 +1,21 @@
+#Exercise 1: Factors
+x = 52633
+for i in range(1, x + 1):  
+    if x % i == 0:          
+        print(i)
+
+
+#Exercise 2: Function
+def print_factor(x):
+    for i in range(1, x + 1):
+        if x % i == 0:
+            print(i)
+
+
+#Exercise 3:List Iteration
+l = [52633, 8137, 1024, 999]
+
+for num in l:
+    print(f"Factors of {num}:")
+    print_factor(num)
+    print("-" * 30)  # 分隔线让输出更清晰
